4525d93f6ec00211e93125d185e1b45a37d2e2ba
XProbie <XProbie@gmail.com>
2016-04-27 23:38:11 +0800

    update main.py

--- renren-crawler/main.py
+++ renren-crawler/main.py
@@ -23,31 +23,5 @@ class test(object):
 		self.userlogin.set_login_info(self.username,self.password)
 		self.login_url = 'http://3g.renren.com/profile.do?id=425186021&sid=PwyGFaedzB81fFMISfrmve&160evn'
 		self.friendlist_url = [self.login_url]
-	def get_friendlist_page(self):
-		url = friendlist_url[0]
-		del friendlist_url[0]
+	def get_page(self,url):
 		return userlogin.login(url)
-
-soup = bs(html_doc)
-
-print soup.prettify()
-'''
-for i in soup.body:
-	try:
-		if i.b != None:
-			print i.b
-	except:
-		print 'None'
-
-for i in soup.body:
-	print i.div
-#urls=re.findall(r"[^?!>]<a.*?profil.*?<\/a>",html_doc)
-'''
-'''
-  <div class="list">
-   <div>
-    <table>
-     <tr valign="top">
-      <td>
-       <a c
-'''
