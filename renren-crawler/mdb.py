003a03e77514ed443139da02fdfbdbf4cd750c2f
XProbie <XProbie@gmail.com>
2016-04-28 11:51:08 +0800

    add ,db.

--- renren-crawler/mdb.py
+++ renren-crawler/mdb.py
@@ -2,6 +2,8 @@
 #-*- coding:utf-8 -*-
 #python 2.7.10
 
+'''数据库操作模块'''
+
 __author__ = 'AJ Kipper'
 
 
@@ -12,23 +14,32 @@ class Engine(object):
 		pass
 
 	def connect(self):
-		con = mdb.connect("localhost","root","ha","crawler")
+		#连接数据库，参数分别是数据库地址，用户名，密码，数据库名字
+		con = mdb.connect("localhost","root","ha","crawler",charset='utf8')
 		cur = con.cursor()
 		self.con = con
 		return cur
 
+	#也许用不到delete操作，但还是写了以免后期要用
 	def delete(self):
 		order = ''
 		cur = self.connect()
 		cur.execute(order)
 		self.con.commit()
 
-	def insert(self):
+	def insert(self,info_list):
 		cur = self.connect()
-		order = 'insert into friends_info values(' + id + '","' + account_id + '","' + user_name + '","' + profile_url + '","' + profile_path'")'
-		cur.execute(order)
+		#获取一个列表参数，将字段信息插入
+		id_count = info_list[0]
+		account_id = info_list[1]
+		user_name = ' '
+		profile_url = info_list[3]
+		#order = 'insert into friends_info values(' + str(id_count) + ',' + str(account_id) + ',' + user_name + ',' + profile_url + ')'
+		#order = "insert into user(id_count,account_id,user_name,profile_url) values(%d,%d,%s,%s)"
+		cur.execute("insert into friends_info values (%d,%d,%s,%s);" % (id_count,account_id,user_name,profile_url))
 		self.con.commit()
 
+	#也许用不到select操作，但还是写了，后期数据分析要用到，所以这里的代码没有具体实现
 	def select(self):
 		cur = self.connect()
 		order = 'select* from friends_info'
