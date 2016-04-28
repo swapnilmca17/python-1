28498c7827a2b6f005f5402da865f2ecd822b3f6
XProbie <XProbie@gmail.com>
2016-04-28 13:37:00 +0800

    add

--- renren-crawler/store_profile.py
+++ renren-crawler/store_profile.py
@@ -2,27 +2,28 @@
 #-*- coding:utf-8 -*-
 #python 2.7.10
 
+'''保存好友主页页面'''
+
 __author__ = 'AJ Kipper'
 
 import os
 
-class test(object):
+class ProfileInfo(object):
 
 	def __init__(self):
+		#判断是否存在profile文件夹
 		if os.path.exists('profile'):
 			pass
 		else:
+			#如果没有则创建一个
 			os.popen('mkdir profile')
 
 	def store(self,user_id,page):
+		#以user_id为名保存在tml文件
 		profile_path = 'profile/' + str(user_id) + '.html'
 		file_obj = open(profile_path,"w+")
 		file_obj.write(page)
 		file_obj.close()
 
 if __name__ == '__main__':
-	file_obj = file('test.html','r+')
-	page = file_obj.read()
-	print page
-	test = test()
-	test.store(1,page)
+	ProfileInfo()
