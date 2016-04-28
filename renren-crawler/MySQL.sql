6105b55bce814e688b8ed47cad4089858f04ad4c
XProbie <XProbie@gmail.com>
2016-04-28 11:47:41 +0800

    add sql

--- renren-crawler/MySQL.sql
+++ renren-crawler/MySQL.sql
@@ -3,9 +3,8 @@ mysql> create database crawler
 Query OK, 1 row affected (0.00 sec)
 
 mysql> create table friends_info(
-    -> id int(4) not null auto_increment primary key,
+    -> id int(4) not null primary key,
     -> account_id int(4) not null,
     -> user_name varchar(20) not null,
-    -> profile_url varchar(80) not null,
-    -> profile_path varchar(80) not null
+    -> profile_url varchar(80) not null
     -> );
