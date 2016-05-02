6babf474d6be15e8db9bf5a73dfdd993dedcf69b
XProbie <XProbie@gmail.com>
2016-05-02 14:04:35 +0800

    add

---
+++ custom-command-line/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding : utf-8-*-
+
+'''Module-function'''
+
+__author__ = 'AJ Kipper'
+
+
+
+
+class test(object):
+    '''class function'''
+    def __init__(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
