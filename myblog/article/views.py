70721a2b5313b794e54ccd4f1d17b0622a574cc8
XProbie <XProbie@gmail.com>
2016-07-03 11:47:28 +0800

    add myblog

---
+++ myblog/article/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+def index(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'index.html', {'post_list' : post_list})
+
+def post_detail(request, post_title):
+    post_list = Article.objects.all()
+    for post in post_list:
+        if post.title == post_title:
+            return render(request, 'post.html', {'post': post})
+        else:
+            pass
+
+
+    # str = ("title = %s, category = %s, date_time = %s, content = %s"
+    #     % (post.title, post.category, post.date_time, post.content))
+    # return HttpResponse(str)
+
+def about(request):
+    return render(request,'about.html')
---
+++ myblog/myblog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'article.views.index'),
+    url(r'^about', 'article.views.about'),
+    # url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^(?P<post_title>.*?)/$', 'article.views.post_detail', name='post_detail'),
+)
